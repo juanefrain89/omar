e7c0f5503914476b1c007a0a5602dbe0b8ab9330
juanefrain89 <302420141@ute.edu.mx>
2024-10-17 11:41:57 -0600

    cambio

--- src/components/tabla/Publicacionespendientes.jsx
+++ src/components/tabla/Publicacionespendientes.jsx
@@ -1,8 +1,15 @@
 import axios from "axios";
+import './tabla.css'
 import { useEffect , useState } from "react";
 const Publicacionespendientes = () => {
     const [loading, setLoading] = useState(true);
-    const [datos, setDatos] = useState([5,5])
+    const [mandar, setmandar ]= useState([])
+    const [datos, setDatos] = useState([ {id:39,
+        placa:'jkhr56',
+        latitud:25.68333908659982,
+        longitud:-100.30677181798846,
+        imagen :'.shkssj',
+        contacto: 8135654041}])
     useEffect(() => {
         axios.get("https://ddcd-5.onrender.com")
           .then(e => {
@@ -15,7 +22,28 @@ const Publicacionespendientes = () => {
 
             setLoading(false);
           });
-      }, []);  
+      }, []); 
+      
+      
+
+      const aceptado = (id)=>{
+        const elementoEncontrado = datos.find(elemento => elemento.id === id);
+
+        axios.post("http://localhost:4200//l", {elemento: elementoEncontrado, operacion: 2 }) 
+            .then(e => {
+            setDatos(e.data);
+            console.log(e.data);            
+            setLoading(false);
+          })
+          .catch(error => {
+            console.log(error);
+
+            setLoading(false);
+          });
+        }
+
+
+
       console.log(datos);
         
     return ( <>
@@ -29,8 +57,8 @@ const Publicacionespendientes = () => {
         <td className="clase">{item.id}</td>
         <td className="clase">{item.placa}</td>
         <td className="clase">{item.imagen}</td>
-        <td className="clase">id</td>
-        <td className="clase">id</td>
+        <td className="clase clasedos"><button className="botoncienaceptar">rechazar</button> <button className="botonrechazar"  onClick={() => aceptado(item.id)}   >aceptar</button> </td>
+       
        </tr>
     )
     
