cae665ee0229d7a377d366b967d8f64c20171f07
juanefrain89 <302420141@ute.edu.mx>
2024-10-17 10:46:24 -0600

    cambio

--- src/components/tabla/Publicacionespendientes.jsx
+++ src/components/tabla/Publicacionespendientes.jsx
@@ -22,11 +22,21 @@ const Publicacionespendientes = () => {
     
     <h1>acepta o rechaza patrullas</h1>
    
-      
-        {datos.map((item)=>{
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta placeat provident consequatur vitae odio perferendis vero expedita. Quia hic optio ea repellendus culpa, nobis a, perspiciatis nisi enim veritatis magni.</p>        
+      <table>  
+    {datos.map((item, index)=>{
+    return(
+       <tr key={index}>
+        <td className="clase">{item.id}</td>
+        <td className="clase">{item.placa}</td>
+        <td className="clase">{item.imagen}</td>
+        <td className="clase">id</td>
+        <td className="clase">id</td>
+       </tr>
+    )
+    
       
         })}
+        </table>
         
         </> );
 }
