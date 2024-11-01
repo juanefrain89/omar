013b87dd5972f43b404d2c732ddf72023166285e
juanefrain89 <302420141@ute.edu.mx>
2024-11-01 00:20:16 -0600

    jdj

--- src/components/patrulla/Patrulla.jsx
+++ src/components/patrulla/Patrulla.jsx
@@ -2,19 +2,19 @@ import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import Inicio from "../inicio/Inicio";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import "./patrulla.css";
 import axios from 'axios';
 import pa from "./diseño.png";
 import Piepag from './pie/Piepag';
-import diseño from './diseño.png'
+import diseño from './diseño.png';
 import { Navigate } from 'react-router-dom';
-import { useContext } from 'react';
 import { Contexto } from '../../General';
+
 const Pattrulla = () => {
-  const  token = localStorage.getItem('token')
-const date = useContext(Contexto)
-console.log(date.datos);
+  const token = localStorage.getItem('token');
+  const date = useContext(Contexto);
+  console.log(date.datos);
 
   const [selectedId, setSelectedId] = useState(null);
   const [datos, setDatos] = useState([]);
@@ -22,39 +22,28 @@ console.log(date.datos);
   const [expandedIndex, setExpandedIndex] = useState(null); 
   const [ver, setVer] = useState(false);
   const [pm, setPm] = useState(null); 
+  const [nuevoarray, setarray] = useState([]);
 
-  
+  const sanitizeImageUrl = (url) => {
+    const match = url.match(/https:\/\/res\.cloudinary\.com\/[^\s]+/);
+    return match ? match[0] : url;
+  };
 
-  const markerPositions = [
-    { lat: 19.432608, lng: -99.133209 },
-    { lat: 19.702556, lng: -101.192376 },
-    { lat: 20.659698, lng: -103.349609 }, 
-    { lat: 25.686614, lng: -100.316113 }, 
-    { lat: 25.683339, lng: -100.306771 }  
-  ];
-const [nuevoarray, setarray]=useState([]);
   const funcionver = (id) => {
     console.log(id);
     
-    const userConfirmed = window.confirm("¿ver patrulla con id: "+ id+ " ?");
-   
-   
-   
+    const userConfirmed = window.confirm("¿ver patrulla con id: " + id + " ?");
     if (userConfirmed) {
-    const elementoEncontrado = datos.find(elemento => elemento.id === id);
-  console.log(elementoEncontrado);
-  
-  if (elementoEncontrado) {
-    setarray(elementoEncontrado);  
-  console.log(nuevoarray , "este es nuevo array");
-  
-  }
-   
-    setPm(id); 
-    setVer(!ver);
-    window.scrollTo(0, 0);  
-    document.body.style.overflow = 'hidden'; 
-}
+      const elementoEncontrado = datos.find(elemento => elemento.id === id);
+      if (elementoEncontrado) {
+        setarray(elementoEncontrado);
+        console.log(nuevoarray, "este es nuevo array");
+      }
+      setPm(id); 
+      setVer(!ver);
+      window.scrollTo(0, 0);  
+      document.body.style.overflow = 'hidden'; 
+    }
   };
 
   const funcionver2 = () => {
@@ -63,10 +52,7 @@ const [nuevoarray, setarray]=useState([]);
   };
 
   useEffect(() => {
-   
-        
-        setDatos(date.datos);
-      
+    setDatos(date.datos);
   }, [date.datos]);
 
   const containerStyle = {
@@ -83,22 +69,20 @@ const [nuevoarray, setarray]=useState([]);
     <>
       {ver ? (
         <div className="absolu">
-        
           <button className='salir' onClick={funcionver2}>x</button>
           <div className="contenedoropciones">
             <div className="one">
-              <div className="imagen"><img src={nuevoarray.imagen} alt="Diseño" /></div>
+              <div className="imagen">
+                <img src={sanitizeImageUrl(nuevoarray.imagen)} alt="Diseño" />
+              </div>
               <div className="carac">
-              <div className="div">  <h1>Placa: </h1> <p>ubx728</p> </div>   
-                <div className="div">  <h1>Dirección: {nuevoarray.unidad}</h1> <p>monterrey</p> </div>               
-                <div className="div">  <h1>Unidad: </h1>  <p>{nuevoarray.latitud}</p> </div>   
-               
-                <div className="div"> <h1>contacto: </h1> <p>{nuevoarray.contacto}</p>   </div>   
-                               
+                <div className="div"><h1>Placa: </h1> <p>ubx728</p></div>   
+                <div className="div"><h1>Dirección: {nuevoarray.unidad}</h1> <p>monterrey</p></div>               
+                <div className="div"><h1>Unidad: </h1> <p>{nuevoarray.latitud}</p></div>   
+                <div className="div"><h1>contacto: </h1> <p>{nuevoarray.contacto}</p></div>   
               </div>
             </div>
           </div>
-     
         </div>
       ) : (
         <Inicio />
@@ -108,34 +92,28 @@ const [nuevoarray, setarray]=useState([]);
         <center>
           <h1 className='patrullash'>Tus patrullas</h1>
         </center>
-                <div className="padre">
-                   {ver === false && (
-              <LoadScript googleMapsApiKey="AIzaSyBT6zx6h6AO_z7D0qHJzql9PvbJ4wDmklc">
-                <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-                  {datos.map((position, index) => (
-               
-                    <Marker
-                      onClick={() => funcionver(position.id)}  
-                      key={index}
-                      position={{ lat: parseFloat(position.latitud.trim()), lng: parseFloat(position.longitud.trim()) }}
-                    />
-                  ))}
-                </GoogleMap>
-              </LoadScript>
-            )}
-          </div>
-          <div className="vbn">
-                  </div>
-             </div>
-           
-           <ul>
-            {date.datos.map((item)=>{
-              return(
-<li>{item.title}</li>
-              )
-            })}
-           </ul>
-           <img src="http://mavahost.com/imagenes/imagen.png" alt="imagen" />
+        <div className="padre">
+          {ver === false && (
+            <LoadScript googleMapsApiKey="AIzaSyBT6zx6h6AO_z7D0qHJzql9PvbJ4wDmklc">
+              <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+                {datos.map((position, index) => (
+                  <Marker
+                    onClick={() => funcionver(position.id)}  
+                    key={index}
+                    position={{ lat: parseFloat(position.latitud.trim()), lng: parseFloat(position.longitud.trim()) }}
+                  />
+                ))}
+              </GoogleMap>
+            </LoadScript>
+          )}
+        </div>
+        <div className="vbn"></div>
+      </div>
+      <ul>
+        {date.datos.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
       <Piepag />
     </>
   );
